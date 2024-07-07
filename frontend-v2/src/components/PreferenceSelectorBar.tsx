305388812b177404b58da063f15692f3740269e7
Wei <wac.weichong@gmail.com>
2024-07-07 12:17:36 +0800

    refactor file structure, add recipe pages

--- frontend-v2/src/components/PreferenceSelectorBar.tsx
+++ frontend-v2/src/components/PreferenceSelectorBar.tsx
@@ -1,3 +1,5 @@
+import PreferenceToggle from "@/components/PreferenceToggle.tsx";
+
 interface PrefenceSelectorBarProps {}
 
 export default function PrefenceSelectorBar({}: PrefenceSelectorBarProps) {
@@ -7,10 +9,7 @@ export default function PrefenceSelectorBar({}: PrefenceSelectorBarProps) {
         "w-[90vw] h-[65px] mx-auto flex flex-row justify-around items-center bg-green-500"
       }
     >
-      <div className={"w-28 h-10 bg-rose-500 rounded-xl"}>High Protein</div>
-      <div className={"w-28 h-10 bg-rose-500 rounded-xl"}>Low Carb</div>
-      <div className={"w-28 h-10 bg-rose-500 rounded-xl"}>Vegetarian</div>
-      <div className={"w-28 h-10 bg-rose-500 rounded-xl"}>Halal</div>
+      <PreferenceToggle />
       {/*<div className={"w-28 h-10 bg-rose-500 rounded-xl"}></div>*/}
       {/*<div className={"w-28 h-10 bg-rose-500 rounded-xl"}></div>*/}
       {/*<div className={"w-28 h-10 bg-rose-500 rounded-xl"}></div>*/}
