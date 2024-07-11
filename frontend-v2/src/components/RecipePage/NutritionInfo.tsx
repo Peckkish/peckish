b975743f7d5a110606bea0b30a7f6976221cb1d4
Wei <wac.weichong@gmail.com>
2024-07-11 18:21:49 +0800

    adjust layout

--- frontend-v2/src/components/RecipePage/NutritionInfo.tsx
+++ frontend-v2/src/components/RecipePage/NutritionInfo.tsx
@@ -29,33 +29,57 @@ export default function NutritionInfo({
         />
       </div>
       <div
-        className={"grid 2xl:min-w-[30vw] min-w-[35vw] min-h-24 gap-3"}
+        className={"grid 2xl:min-w-[35vw] min-w-[40vw] min-h-24 gap-3"}
         style={{
-          gridTemplateColumns: "repeat(auto-fit, minmax(min(50px, 100%), 1fr))",
+          gridTemplateColumns: "repeat(auto-fit, minmax(min(80px, 100%), 1fr))",
         }}
       >
-        <div className={"flex flex-col items-center border-2 pt-4 rounded-lg"}>
+        <div
+          className={
+            "flex flex-col items-center border-2 border-[#33E14D]/50 brightness-75 pt-4 rounded-lg"
+          }
+        >
           <p className={"font-medium text-md"}>Energy</p>
           <p className={"font-light mt-2 text-sm"}>501cal</p>
           <p className={"font-light text-sm"}>(2012kj)</p>
         </div>
-        <div className={"flex flex-col items-center border-2 pt-4 rounded-lg"}>
+        <div
+          className={
+            "flex flex-col items-center border-2 border-[#33E14D]/50 brightness-75 pt-4 rounded-lg"
+          }
+        >
           <p className={"font-medium text-md"}>Protein</p>
           <p className={"font-light mt-2 text-sm"}>38.4g</p>
         </div>
-        <div className={"flex flex-col items-center border-2 pt-4 rounded-lg"}>
+        <div
+          className={
+            "flex flex-col items-center border-2 border-[#33E14D]/50 brightness-75 pt-4 rounded-lg"
+          }
+        >
           <p className={"font-medium text-md"}>Fat</p>
           <p className={"font-light mt-2 text-sm"}>15.3g</p>
         </div>
-        <div className={"flex flex-col items-center border-2 pt-4 rounded-lg"}>
+        <div
+          className={
+            "flex flex-col items-center border-2 border-[#33E14D]/50 brightness-75 pt-4 rounded-lg"
+          }
+        >
           <p className={"font-medium text-md"}>Carbs</p>
           <p className={"font-light mt-2 text-sm"}>56.3g</p>
         </div>
-        <div className={"flex flex-col items-center border-2 pt-4 rounded-lg"}>
+        <div
+          className={
+            "flex flex-col items-center border-2 border-[#33E14D]/50 brightness-75 pt-4 rounded-lg"
+          }
+        >
           <p className={"font-medium text-md"}>Fibre</p>
           <p className={"font-light mt-2 text-sm"}>3g</p>
         </div>
-        <div className={"flex flex-col items-center border-2 pt-4 rounded-lg"}>
+        <div
+          className={
+            "flex flex-col items-center border-2 border-[#33E14D]/50 brightness-75 pt-4 rounded-lg"
+          }
+        >
           <p className={"font-medium text-md"}>Salt</p>
           <p className={"font-light mt-2 text-sm"}>400mg</p>
         </div>
